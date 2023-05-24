04bf0cd9c3ea2d4d7c9ae7f51e6aa1501382ed19
jumavictor <victorjuma48@gmail.com>
2023-05-24 01:32:17 -0700

    Task 1. a Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack.

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,37 @@
+#!/usr/bin/python3
+"""script that generates a .tgz archive from the contents of the web_static
+folder of your AirBnB Clone repo, using the function do_pack.
+"""
+from datetime import datetime
+from fabric.api import local
+import os
+
+
 def do_pack():
-    # Create the versions folder if it doesn't exist
-    if not os.path.exists("versions"):
-        os.mkdir("versions")
+    """ generates a .tgz archive from the contents of the web_static
 
-    # Get the current date and time
-    now = datetime.now()
+    All files in the folder web_static must be added to the final archive.
+    All archives must be stored in the folder versions.
+    The name of the archive created must be:
+        web_static_<year><month><day><hour><minute><second>.tgz
+    The function do_pack must return the archive path if the archive has
+    been correctly generated. Otherwise, it should return None.
 
-    # Create the archive name
-    archive_name = "web_static_{}-{}-{}-{}-{}-{}".format(
-        now.year, now.month, now.day, now.hour, now.minute, now.second
-    )
+    Returns:
+        fabric.operations._AttributeString: archive path.
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    # Create the archive
-    with tarfile.open(archive_name + ".tgz", "w:gz") as archive:
-        for file in os.listdir("web_static"):
-            archive.add("web_static/{}".format(file))
+    # create folder versions if it doesn’t exist
+    local("mkdir -p versions")
 
-    # Return the archive path
-    return archive_name + ".tgz"
+    # extract the contents of a tar archive
+    result = local("tar -czvf versions/web_static_{}.tgz web_static"
+                   .format(now))
+    if result.failed:
+        return None
+    else:
+        return result
 
+if __name__ == "__main__":
+    do_pack()
