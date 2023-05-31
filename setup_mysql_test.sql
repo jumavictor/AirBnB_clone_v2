f14d40340a2c45605e2db0b01191b9525b6d8338
jumavictor <victorjuma48@gmail.com>
2023-05-31 01:24:02 -0700

    test updates

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,12 @@
--- script that prepares a dev MySQL server for the AirBnB clone project.
+-- this script prepares a MySQL server for the project
+-- create project testing database with the name : hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- a new user hbnb_test (in localhost).
--- the password of hbnb_test should be set to hbnb_test_pwd.
+-- creating new user named : hbnb_test with all privileges on the db hbnb_test_db
+-- with the password : hbnb_test_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
--- hbnb_test should have all privileges on the database hbnb_test_db only.
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
--- hbnb_test should have SELECT privilege on the database performance_schema.
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+-- granting the SELECT privilege for the user hbnb_test on the db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+-- granting all privileges to the new user on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
